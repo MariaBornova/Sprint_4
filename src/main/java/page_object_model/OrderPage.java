ed9640d0af05378df12c3930179b8811b41c2002
Stas Basov <mchebotarenko@yandex.com>
2024-02-24 21:24:23 +0500

    Tests for project Samokat

--- src/main/java/page_object_model/OrderPage.java
+++ src/main/java/page_object_model/OrderPage.java
@@ -1,7 +1,6 @@
 package page_object_model;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
